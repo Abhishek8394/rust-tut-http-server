f8e2d4ebf3dbfe7cb59d5da06eb12018161fdd2a
Abhishek Patel <5524161+Abhishek8394@users.noreply.github.com>
2020-07-15 20:42:37 -0700

    single threaded server

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
-use std::net::TcpListener;
+use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
+    println!("Listening..");
     for stream in listener.incoming(){
         let stream = stream.unwrap();
-        println!("Connection established");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    // println!("Request\n{}", String::from_utf8_lossy(&buffer[..]));
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status, filename) = if buffer.starts_with(get){
+        ("HTTP/1.1 200 OK \r\n\r\n", "index.html")
+    }
+    else{
+      ("HTTP/1.1 400 Not found \r\n\r\n", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status, contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush();
 }
