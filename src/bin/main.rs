53f1f2fc8361662826648ea7f5cf9ffb33f49e41
Abhishek Patel <5524161+Abhishek8394@users.noreply.github.com>
2020-07-21 23:48:19 -0700

    rust fmt

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,43 +1,40 @@
 use http_server::ThreadPool;
-use std::thread;
-use std::time::Duration;
 use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
     let pool = ThreadPool::new(4).unwrap();
     println!("Listening..");
-    for stream in listener.incoming().take(2){
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        pool.execute(||{
+        pool.execute(|| {
             handle_connection(stream);
         });
-        
     }
 
     println!("Shutting down...");
 }
 
-fn handle_connection(mut stream: TcpStream){
+fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
     // println!("Request\n{}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
-    let (status, filename) = if buffer.starts_with(get){
+    let (status, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK \r\n\r\n", "index.html")
-    }
-    else if buffer.starts_with(sleep){
+    } else if buffer.starts_with(sleep) {
         println!("sleeping for 5.");
         thread::sleep(Duration::from_secs(5));
         println!("I'm up.");
         ("HTTP/1.1 200 OK \r\n\r\n", "index.html")
-    }
-    else{
-      ("HTTP/1.1 400 Not found \r\n\r\n", "404.html")
+    } else {
+        ("HTTP/1.1 400 Not found \r\n\r\n", "404.html")
     };
     let contents = fs::read_to_string(filename).unwrap();
     let response = format!("{}{}", status, contents);
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-use std::thread;
 use std::fmt;
-use std::sync::{mpsc,Arc,Mutex};
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
 
 /// Custom Pool creation error
 #[derive(Debug)]
 pub struct PoolCreationError;
 
-impl fmt::Display for PoolCreationError{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+impl fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Error creating pool. Size must be > 1")
     }
 }
@@ -17,28 +17,28 @@ type Job = Box<dyn FnOnce() + Send + 'static>;
 
 /// Workers listen for jobs indefinitely. We need to send some commands like stop
 /// Below enum wraps commands for workers.
-enum Message{
+enum Message {
     NewJob(Job),
-    Terminate
+    Terminate,
 }
 
 /// Pool worker.
-struct Worker{
+struct Worker {
     id: String,
     thread: Option<thread::JoinHandle<()>>,
 }
 
-impl Worker{
-    fn new(id: String, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker{
+impl Worker {
+    fn new(id: String, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let id_copy = String::from(&id[..]);
-        let thread = thread::spawn(move||{
+        let thread = thread::spawn(move || {
             loop {
                 let msg = receiver.lock().unwrap().recv().unwrap();
-                match msg{
+                match msg {
                     Message::NewJob(job) => {
                         println!("Worker {} got job.", id_copy);
                         job();
-                    },
+                    }
                     Message::Terminate => {
                         println!("Worker {} got terminate signal.", id_copy);
                         break;
@@ -47,60 +47,64 @@ impl Worker{
             }
             println!("Worker: {} has stopped serving requests", id_copy);
             // Below will make our server serial!
-            // This is because the mutex lock obtained will remain in scope for duration of block 
+            // This is because the mutex lock obtained will remain in scope for duration of block
             // which is when the job runs. So we would have to lock for entire duration of job!
             // But we only need to pop job from queue, dont need lock later. Hence the `loop`
             // block above, it releases lock as soon as the statement finishes executing.
             // Try using below block as main loop, requests will be served serially!
-            
+
             // while let Ok(job) = receiver.lock().unwrap().recv(){
             //     println!("Worker {} got job.", id_copy);
             //     job();
             // }
         });
-        Worker{id, thread: Some(thread)}
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
-
 }
 
-
-pub struct ThreadPool{
+pub struct ThreadPool {
     workers: Vec<Worker>,
     sender: mpsc::Sender<Message>,
 }
 
-impl ThreadPool{
+impl ThreadPool {
     /// Create a new threadpool
     ///
     ///
     /// The size is number of workers in pool
-    /// 
+    ///
     /// # Panics
-    /// 
+    ///
     /// `new` will panic if size is 0
-    pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError>{
+    pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError> {
         if size == 0 {
             return Result::Err(PoolCreationError);
         }
         let mut workers = Vec::with_capacity(size);
         let (sender, receiver) = mpsc::channel();
         let receiver = Arc::new(Mutex::new(receiver));
-        for i in 0..size{
+        for i in 0..size {
             workers.push(Worker::new(format!("worker-{}", i), Arc::clone(&receiver)));
         }
-        return Result::Ok(ThreadPool{workers, sender});
+        return Result::Ok(ThreadPool { workers, sender });
     }
-    
-    pub fn execute<F>(&self, f: F)  where F: FnOnce() + Send + 'static{
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
         let job = Box::new(f);
         self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
-impl Drop for ThreadPool{
-    fn drop(&mut self){
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
         println!("Sending terminate signal to all workers");
-         for _ in &self.workers {
+        for _ in &self.workers {
             self.sender.send(Message::Terminate).unwrap();
             // you cannot join on worker immediately here because
             // the message can be consumed by some other worker!
@@ -108,9 +112,9 @@ impl Drop for ThreadPool{
         }
         println!("Shutting down workers");
 
-        for worker in &mut self.workers{
+        for worker in &mut self.workers {
             println!("Shutting down worker: {}", worker.id);
-            if let Some(thread) = worker.thread.take(){
+            if let Some(thread) = worker.thread.take() {
                 thread.join().unwrap();
             }
         }
